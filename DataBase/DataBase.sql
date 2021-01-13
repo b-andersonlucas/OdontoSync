a048b1e97105816a09f87b946663ba6894a94e0e
Dickson Teixeira <43281367+DicksonSilva@users.noreply.github.com>
2021-01-13 00:11:20 -0200

    Organização do BD, HTML e da conexão PHP <-> BD.
    - Banco de dados
        • Verificou-se os atributos
        • Verificou-se as inserções de teste
        • Foi testado o BD localmente
    - HTML
        • Verificar métodos de envio
        • Padronizar os parâmetros
        • Controlar o envio de dados
    - PHP (Conexão)
        • Criou-se o arquivo de conexão DB
        • Criou-se o arquivo de *logout*
        • Criou-se o arquivo para processar

--- DataBase/DataBase.sql
+++ DataBase/DataBase.sql
@@ -2,37 +2,34 @@ create database IF not exists OdontoSync;
 
 use OdontoSync;
 
-create table Cliente(
-id int(10) primary key NOT null auto_increment,
-nome varchar(35),
-telefone char(15),
-cidade char(20),
-data_nascimento char(12),
-login char(20),
-senha char(15)
+create table Pessoa(
+email  VARCHAR (50)  primary key NOT null,
+usuario char(20) NOT null,
+senha char(15) NOT null,
+priv_pessoa char(1) NOT null,
+nome varchar(35) NOT null,
+telefone char(15)
 );
-
-create table Dentista(
-id int(10) primary key not null,
-nome varchar(35),
-telefone char(15),
-numero_registro char(10),
-login char(20),
-senha char(15)
+ 
+create table Cliente(
+pessoa_email VARCHAR (50),
+FOREIGN KEY  (pessoa_email) REFERENCES Pessoa(email) ,
+cidade char(20),
+data_nascimento char(12)
 );
 
 create table Secretaria(
-id int(10) primary key not null,
-nome varchar(35),
-telefone char(15),
-login char(20),
-senha char(15)
+pessoa_email VARCHAR (50),
+FOREIGN KEY (pessoa_email) REFERENCES Pessoa(email),
+data_vinculo char(20)
+);
+
+create table Dentista(
+pessoa_email VARCHAR (50),
+FOREIGN KEY (pessoa_email) REFERENCES Pessoa(email),
+codigo_registro char(10)
 );
 
 
-insert into Cliente(id,nome,telefone,cidade,data_nascimento,login,senha) values(1,'OdontoSyncManager','9xxxx-xxxx','Santo Antônio,RN','01/12/2020','admin','admin');
-insert into Cliente(id,nome,telefone,cidade,data_nascimento,login,senha) values(101,'nulo','9xxxx-xxxx','not found','00/00/0000','usernull','senhanull');
--- insert into Cliente(nome,telefone,cidade,data_nascimento,login,senha) values();
-
--- select * from Cliente;
-
+insert into Pessoa values('d@i.o', 'dickson', '123', 'c', 'dickson teixeira', '(84) 99999-9999');
+insert into Cliente values ('d@i.o', 'serrinha', '22/02/2002');

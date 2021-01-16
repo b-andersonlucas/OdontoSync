302a4868cde95944d5c689d90541e1aba0cc7063
Dickson Teixeira <43281367+DicksonSilva@users.noreply.github.com>
2021-01-16 00:57:09 -0200

    Implementação da autenticação da sessão (login) e do DashBoard alpha.
    - PHP (Processamento)
        • Foram identificados os dados recebidos
        • Os dados recebidos foram tratados
        • Incluiu-se o arquivo de conexão
        • Realizou-se a consulta ao BD para a autenticação
        • Foi verificado o retorno da consulta
        • Criou-se uma sessão de autenticação
        • Foram enviados dados através da sessão
        • Foram resolvidos os redirecionamentos
    - PHP (DashBoard)
        • O arquivo de DashBoard foi criado
        • Recuperou-se os dados armazenados na sessão
        • Gerou-se a página alpha do DashBoard
    
    Co-Authored-By: Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>

--- src/assets/php/process.php
+++ src/assets/php/process.php
@@ -1,1 +1,38 @@
-<?php
+<?php 
+
+session_start();
+if (isset($_SESSION["autenticado"])) {
+    
+    header("location:/odontosync/src/index.html");
+}
+
+session_destroy();
+
+if (isset($_POST["ok"])) {
+    $user = isset($_POST["user"])?$_POST["user"]:"";
+    $password = isset($_POST["password"])?$_POST["password"]:"";
+
+    include('connection.php');
+
+    $sql = "SELECT email, nome, priv_pessoa FROM pessoa WHERE usuario = '$user' and senha = '$password'";
+
+    $resultado = mysqli_query($con, $sql);
+
+    mysqli_close($con);
+ 
+
+    if (mysqli_num_rows($resultado) > 0){
+        session_start();
+        $linha = mysqli_fetch_array($resultado, MYSQLI_BOTH);
+        $_SESSION["autenticado"] = true;
+        $_SESSION["email"] = $linha["email"];
+        $_SESSION["nome"] = $linha["nome"];
+        $_SESSION["priv_pessoa"] = $linha["priv_pessoa"];
+        header("location:/odontosync/src/pages/dashboard.php");
+    } else {
+        header("location:/odontosync/src/pages/login.html");
+    }
+   
+} else {
+    header("location:/odontosync/src/index.html");
+}

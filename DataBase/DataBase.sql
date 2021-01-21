6562b8d045388494015d05437f8ecff62061297a
José Gabriel de Aquino Vicente <gabrielvycente2016@outlook.com.br>
2021-01-21 17:08:14 -0300

    atualização no banco de dados
    adição de novas tabelas + linhas para verificação de funcionamento das tabelas+ linhas de adição exemplo

--- DataBase/DataBase.sql
+++ DataBase/DataBase.sql
@@ -15,7 +15,7 @@ create table Cliente(
 pessoa_email VARCHAR (50),
 FOREIGN KEY  (pessoa_email) REFERENCES Pessoa(email) ,
 cidade char(20),
-data_nascimento char(12)
+data_nascimento date
 );
 
 create table Secretaria(
@@ -30,6 +30,45 @@ FOREIGN KEY (pessoa_email) REFERENCES Pessoa(email),
 codigo_registro char(10)
 );
 
+create table Historico(
+Cliente_email VARCHAR (50),
+FOREIGN KEY  (Cliente_email) REFERENCES Cliente(pessoa_email),
+dentista_email VARCHAR (50),
+FOREIGN KEY (dentista_email) REFERENCES Dentista(pessoa_email),
+dia date,
+hora time,
+procedimento varchar(450)
+);
+
+create table Agenda_dentista(
+dentista_email VARCHAR (50),
+FOREIGN KEY (dentista_email) REFERENCES Dentista(pessoa_email),
+dia date,
+n_clientes int(5)
+);
+
+create table Horarios(
+Cliente_email VARCHAR (50),
+FOREIGN KEY  (Cliente_email) REFERENCES Cliente(pessoa_email),
+dentista_email VARCHAR (50),
+FOREIGN KEY (dentista_email) REFERENCES Dentista(pessoa_email),
+dia date,
+horario time
+);
 
 insert into Pessoa values('d@i.o', 'dickson', '123', 'c', 'dickson teixeira', '(84) 99999-9999');
-insert into Cliente values ('d@i.o', 'serrinha', '22/02/2002');
+insert into Cliente values ('d@i.o', 'serrinha', '2002/02/22');
+insert into Pessoa values('a@a.o', 'cavalo', '123', 'd', 'zé', '(00) 90000-0000');
+insert into Dentista values('a@a.o','0003222');
+insert into Historico values('d@i.o','a@a.o','21/01/15','17:30','arranquei o dente pela raiz');
+insert into Agenda_dentista values('a@a.o','2021/01/15',6);
+insert into Horarios values('d@i.o','a@a.o','21/01/15','17:30');
+
+-- select * from Pessoa;
+-- select * from Cliente;
+-- select * from Dentista;
+-- select * from Historico;
+-- select * from Agenda_dentista;
+-- select * from Horarios;
+-- select * from ;
+-- drop database odontosync;

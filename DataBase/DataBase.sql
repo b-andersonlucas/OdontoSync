cfb6e4952cc5ddf65bd52b02402dea1135b76160
José Gabriel de Aquino Vicente <gabrielvycente2016@outlook.com.br>
2021-02-04 11:10:41 -0300

    cadastro na tabela Agenda_dentista sendo efetuado com sucesso + mudanças no banco de dados
    como o titulo já fala está tudo ok porém ainda tem que ajeitar a parte do login, para poder usar o email do destista que está logado, já que por enquanto estamos atribuindo um email à uma variavel para testes e também tem que ajeitar a parte da sessão pois toda vez que o cadastro é feito, a pagina dá um reloading.

--- DataBase/DataBase.sql
+++ DataBase/DataBase.sql
@@ -43,8 +43,10 @@ procedimento varchar(450)
 create table Agenda_dentista(
 dentista_email VARCHAR (50),
 FOREIGN KEY (dentista_email) REFERENCES Dentista(pessoa_email),
-dia date,
-n_clientes int(5)
+dia date primary key,
+n_clientes int(5),
+inicio_ex time,
+fim_ex time
 );
 
 create table Horarios(
@@ -52,7 +54,7 @@ Cliente_email VARCHAR (50),
 FOREIGN KEY  (Cliente_email) REFERENCES Cliente(pessoa_email),
 dentista_email VARCHAR (50),
 FOREIGN KEY (dentista_email) REFERENCES Dentista(pessoa_email),
-dia date,
+dia date primary key,
 horario time
 );
 
@@ -61,14 +63,18 @@ insert into Cliente values ('d@i.o', 'serrinha', '2002/02/22');
 insert into Pessoa values('a@a.o', 'cavalo', '123', 'd', 'zé', '(00) 90000-0000');
 insert into Dentista values('a@a.o','0003222');
 insert into Historico values('d@i.o','a@a.o','21/01/15','17:30','arranquei o dente pela raiz');
-insert into Agenda_dentista values('a@a.o','2021/01/15',6);
+insert into Agenda_dentista values('a@a.o','2021/01/15',6,'08:30','18:30');
 insert into Horarios values('d@i.o','a@a.o','21/01/15','17:30');
 
- select * from Pessoa;
+
+-- select * from Pessoa;
 -- select * from Cliente;
 -- select * from Dentista;
 -- select * from Historico;
- select * from Agenda_dentista;
+-- select * from Agenda_dentista;
 -- select * from Horarios;
 -- select * from ;
+
+-- delete from Agenda_dentista where dia='2021/02/05';
+
 -- drop database odontosync;
--- src/assets/php/process_agenda.php
+++ src/assets/php/process_agenda.php
@@ -1,48 +1,69 @@
 <?php
 
-session_start();
+/*session_start();
 if (isset($_SESSION["autenticado"])) {
     
     header("location:/odontosync/src/index.html");
 }
 
 session_destroy();
-
+*/
 if(isset($_POST['ok'])){
-
-
 
     //String Dcont = dataN.substring(0,2);
     //String Mcont = dataN.substring(3,5);
     //String Acont = dataN.substring(6);
 
-
+    /*
     $email = isset($_POST[""])?$_POST[""]:"";
-    $email1 = " a@a.o";
+    $email1 = "a@a.o";
+    $dataInicial=$_POST["serviceDay"];
+    $inicoEX = $_POST["starHour"];
+    $fimEX = $_POST["endHour"];
+    $numeroPacientes = $_POST["numClients"];
+    */
+    
+    /*
+    $dataAno= substr($dataInicial, 0, 4) ;
+    $dataMes= substr($dataInicial, 5, 6);
+    $dataDia= substr($dataInicial, 8);
+    */
+    //$dataFinal = $dataAno."/".$dataMes."/".$dataDia;
+    
+    //testando as variaveis
+    /* 
+    echo $email1."<br>";
+    echo $dataFinal."essa é a data final <br>";
+    echo $dataInicial."essaa é a data incial<br>";
+    echo $inicoEX."<br>";
+    echo $fimEX."<br>";
+    echo $numeroPacientes."<br>";
+    */
+    //echo $sql;
+    //$sql1 = "insert into Agenda_dentista values('{$email1}','{$dataFinal}',$numeroPacientes);";
+    //$conexao = mysqli_connect('localhost', 'root', '', 'OdontoSync');
+    // insert into Agenda_dentista values('a@a.o','2021/01/15',6);
+    
+    include('connection.php');
+    
+    $email = isset($_POST[""])?$_POST[""]:"";
+    $email1 = "a@a.o";
     $dataInicial = isset($_POST["serviceDay"])?$_POST["serviceDay"]:"";
     $inicoEX = isset($_POST["starHour"])?$_POST["starHour"]:"";
-    $fimEX = isset($_POST[""])?$_POST[""]:"";
+    $fimEX = isset($_POST["endHour"])?$_POST["endHour"]:"";
     $numeroPacientes = isset($_POST["numClients"])?$_POST["numClients"]:"";
+    
+    $dataFinal = str_replace("-","/",$dataInicial);
 
-    $dataDia= substr($dataInicial, 0, 2) ;
-    $dataMes= substr($dataInicial, 3, 5);
-    $dataAno= substr($dataInicial, 6);
+    $sql = "insert into Agenda_dentista values('$email1','$dataFinal',$numeroPacientes,'$inicoEX','$fimEX');";
 
-    $dataFinal = $dataAno."/".$dataMes."/".$dataDia;
+    mysqli_query($con,$sql) or die (" falha ao inserir ");
 
-    include('connection.php');
-
-
-    $sql = "insert into Agenda_dentista values('{$email}','{$dataFinal}','{$inicoEX}','{$fimEX}',$numeroPacientes);";
-    $sql1 = "insert into Agenda_dentista values('{$email1}','{$dataFinal}',$numeroPacientes);";
-    $conexao = mysqli_connect('localhost', 'root', '', 'OdontoSync');
-
-    mysqli_query($conexao,$sql1) or die (" falha ao inserir :".mysqli_error(""));
-
+    header("location:/odontosync/src/pages/page-admin.html");
 
 }
 
 
-// insert into Agenda_dentista values('a@a.o','2021/01/15',6);
+
 
 ?>
